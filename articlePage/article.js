76ca45b5d2ca7a1b1caef87509fad3439e29f4d8
Shivesh Nandan <kshivesh929@gmail.com>
2022-08-28 14:00:59 +0530

    article page

---
+++ articlePage/article.js
@@ -0,0 +1,17 @@
+// alert("hi")
+$(".bttttn").hide();
+$(".btttn").on("click",function(){
+    $("p").removeClass("tohide");
+    $(".bttttn").show();
+    $(".btttn").hide();
+    console.log("hi");
+});
+$(".bttttn").on("click",function(){
+    $("p").addClass("tohide");
+    $(".btttn").show();
+    $(".bttttn").hide();
+    console.log("bye");
+});
+
+
+
